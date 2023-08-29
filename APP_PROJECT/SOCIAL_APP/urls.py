9847e2f79e3b55c3bfe3b6904ca85ccba6809845
akhildiddi <akhilsubscriptions8@gmail.com>
2023-08-29 14:57:34 +0530

    added file

--- APP_PROJECT/SOCIAL_APP/urls.py
+++ APP_PROJECT/SOCIAL_APP/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage, name='homepage')
+    path('', views.register, name='register'),
+    path('login/', views.custom_login, name='login'),
+    path('account/', views.account, name='account'),
+    path('submit_activity/', views.submit_activity, name='submit_activity'),
 ]
+
--- APP_PROJECT/SOCIAL_APP/views.py
+++ APP_PROJECT/SOCIAL_APP/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-def homepage(request):
-    return render(request,"index.html")
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')  # Redirect to login page after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
+
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page or wherever you want
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'login.html')
+
+from django.contrib.auth.decorators import login_required
+from .models import UserActivity
+
+@login_required
+def account(request):
+    user_activities = UserActivity.objects.filter(user=request.user)
+    return render(request, 'account.html', {'user_activities': user_activities})
+
+@login_required
+def submit_activity(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        UserActivity.objects.create(user=request.user, content=content)
+    return redirect('account')
